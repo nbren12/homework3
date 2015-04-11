18c3a8fa780bf5e915484cec5a992a2513335472
Noah D. Brenowitz <nbren12@gmail.com>
2015-04-11 13:49:14 -0400

    remove over relaxed stuff

--- gs.c
+++ gs.c
@@ -3,7 +3,6 @@
 #include <math.h>
 
 #define STOP_ITER_RAT 10e-6
-#define OMEGA_OVER_RELAXED 1.5
 
 
 double calc_resid(int N, double* f, double *u){
@@ -40,27 +39,6 @@ void gauss_seidel_laplace(int N, double *f, double *u){
   u[N-1] = (h2* f[N-1] + u[N-2])/2.0;
 }
 
-void overrelaxed_gauss_seidel_laplace(int N, double *f, double *u, double* w){
-  int i;
-  double h2 = 1.0/(N+1)/(N+1);
-  
-  for (i = 0; i < N; i++)
-    w[i] = u[i];
-
-
-  u[0] =  (h2 * f[0] + u[1])/2.0;
-
-  for (i = 1; i < N-1; i++) {
-    u[i] = (h2 * f[i] + u[i-1] + u[i+1])/2.0;
-  }
-
-  u[N-1] = (h2* f[N-1] + u[N-2])/2.0;
-  
-  for (i = 0; i < N; i++)
-    u[i] = (1-OMEGA_OVER_RELAXED) * w[i] + OMEGA_OVER_RELAXED * u[i];
-
-}
-
 void test_resid(){
   int N = 100;
   
@@ -87,8 +65,6 @@ void test_resid(){
 int main(int argc, char *argv[])
 {
   
-  const int run_over_relaxed = 0; // if not 0 run with overrelaxed version of algorithm
-  
 
   int N = atoi(argv[1]);
   
@@ -97,10 +73,6 @@ int main(int argc, char *argv[])
   // allocate arrays
   u = (double *) malloc(N*sizeof (double));
   f = (double *) malloc(N*sizeof (double));
-  
-  if (run_over_relaxed){
-    w = (double *) malloc(N*sizeof (double));
-  }
   
   // initialize f and u
   int i;
@@ -116,11 +88,7 @@ int main(int argc, char *argv[])
   
   while (resid_cur / resid_init > STOP_ITER_RAT){
     
-    if (run_over_relaxed){
-      overrelaxed_gauss_seidel_laplace(N, f, u, w);
-    } else {
-      gauss_seidel_laplace(N, f, u);
-    }
+    gauss_seidel_laplace(N, f, u);
 
     resid_cur = calc_resid(N, f, u);
     printf("Resid is %f\n", resid_cur );
