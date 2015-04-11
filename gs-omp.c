3f82fd5f5d755879c8cae3161718253bda71a9c2
Noah D. Brenowitz <nbren12@gmail.com>
2015-04-11 17:13:34 -0400

    parallel version done
    Needs timing

--- gs-omp.c
+++ gs-omp.c
@@ -3,7 +3,6 @@
 #include <math.h>
 
 #define STOP_ITER_RAT 10e-6
-#define OMEGA_OVER_RELAXED 1.5
 
 
 double calc_resid(int N, double* f, double *u){
@@ -13,13 +12,8 @@ double calc_resid(int N, double* f, double *u){
 
   double h2 = 1.0/(N+1)/(N+1);
   
-  ai  =  (2 * u[0] - u[1])/h2 -f[0];
-  resid += ai*ai;
-
-  ai =  (2 * u[N-1] - u[N-2])/h2 -f[N-1];
-  resid += ai*ai;
-
-  for (i = 1; i < N-1; i++) {
+#pragma omp parallel for reduction(+:resid) 
+  for (i = 1; i < N+1; i++) {
     ai = (- u[i+1] + 2 * u[i] - u[i-1])/h2 -f[i];
     resid += ai*ai;
   }
@@ -27,38 +21,14 @@ double calc_resid(int N, double* f, double *u){
   return sqrt(resid);
 }
 
-void gauss_seidel_laplace(int N, double *f, double *u){
+void gauss_seidel_laplace_redblack(int N, int start, double *f, double *u){
   int i;
   double h2 = 1.0/(N+1)/(N+1);
 
-  u[0] =  (h2 * f[0] + u[1])/2.0;
-
-  for (i = 1; i < N-1; i++) {
+  #pragma omp parallel for
+  for (i = 1 + start; i < N+1; i+=2) {
     u[i] = (h2 * f[i] + u[i-1] + u[i+1])/2.0;
   }
-
-  u[N-1] = (h2* f[N-1] + u[N-2])/2.0;
-}
-
-void overrelaxed_gauss_seidel_laplace(int N, double *f, double *u, double* w){
-  int i;
-  double h2 = 1.0/(N+1)/(N+1);
-  
-  for (i = 0; i < N; i++)
-    w[i] = u[i];
-
-
-  u[0] =  (h2 * f[0] + u[1])/2.0;
-
-  for (i = 1; i < N-1; i++) {
-    u[i] = (h2 * f[i] + u[i-1] + u[i+1])/2.0;
-  }
-
-  u[N-1] = (h2* f[N-1] + u[N-2])/2.0;
-  
-  for (i = 0; i < N; i++)
-    u[i] = (1-OMEGA_OVER_RELAXED) * w[i] + OMEGA_OVER_RELAXED * u[i];
-
 }
 
 void test_resid(){
@@ -87,24 +57,21 @@ void test_resid(){
 int main(int argc, char *argv[])
 {
   
-  const int run_over_relaxed = 0; // if not 0 run with overrelaxed version of algorithm
-  
 
   int N = atoi(argv[1]);
+  int maxIter, iter;
+  maxIter = atoi(argv[2]);
+  iter = 0;
   
   double *u, *f, *w;
 
   // allocate arrays
-  u = (double *) malloc(N*sizeof (double));
-  f = (double *) malloc(N*sizeof (double));
-  
-  if (run_over_relaxed){
-    w = (double *) malloc(N*sizeof (double));
-  }
+  u = (double *) malloc((N+2)*sizeof (double));
+  f = (double *) malloc((N+2)*sizeof (double));
   
   // initialize f and u
   int i;
-  for (i = 0; i < N; i++) {
+  for (i = 0; i < N+2; i++) {
     f[i] = 1.0;
     u[i] = 0.0;
   }
@@ -115,15 +82,14 @@ int main(int argc, char *argv[])
   resid_cur = resid_init;
   
   while (resid_cur / resid_init > STOP_ITER_RAT){
+    u[0] = u[N+1] = 0.0;
     
-    if (run_over_relaxed){
-      overrelaxed_gauss_seidel_laplace(N, f, u, w);
-    } else {
-      gauss_seidel_laplace(N, f, u);
-    }
+    gauss_seidel_laplace_redblack(N, 0, f, u);
+    gauss_seidel_laplace_redblack(N, 1, f, u);
 
     resid_cur = calc_resid(N, f, u);
     printf("Resid is %f\n", resid_cur );
+    if (++iter >= maxIter ) break;
   }
 
   // deallocate
